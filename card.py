7ce3766efbf37d593ee0694d51cd995b07aaad59
Piotr <kierzekp@gmail.com>
2021-08-10 08:45:23 +0200

    refactored card types into a seperate file

--- card.py
+++ card.py
@@ -1,18 +1,11 @@
 from typing import List, Set, Iterable
+
+from card_types import CardType
 from utils import StringableEnum
 
 
-class CardType(StringableEnum):
-    PERMANENT_LAND = 0
-    PERMANENT_CREATURE = 1
-    PERMANENT_ARTIFACT = 2
-    PERMANENT_ENCHANTMENT = 3
-    PERMANENT_PLANESWALKER = 4
-    SPELL_INSTANT = 10
-    SPELL_SORCERY = 11
-
-
 class Color(StringableEnum):
+    """Enumeration of mana colors in Magic the Gathering."""
     ANY = -1
     WHITE = 0
     BLUE = 1
@@ -22,25 +15,34 @@ class Color(StringableEnum):
 
 
 class CardRarity(StringableEnum):
+    """Enumeration of possible card rarities in Magic the Gathering."""
     COMMON = 0
     UNCOMMON = 1
     RARE = 2
     MYTHIC_RARE = 3
+    BONUS = 4
 
 
 class ManaPoint:
+    """A class abstracting a single mana point as drawn on a card."""
     def __init__(self, colors: List[Color]):
         self.colors = colors
 
 
 class Card:
-    def __init__(self, name: str, mana_cost: List[ManaPoint], types: List[CardType], rarity: CardRarity):
+    """A class abstracting a physical, static Magic the Gathering card. All the costs, abilities and effects are
+    represented exactly as on the card, and do not take into account any effects that affect it during game. """
+    def __init__(self,
+                 name: str, mana_cost: List[ManaPoint], types: List[CardType], rarity: CardRarity,
+                 base_attack: int = 0, base_defence: int = 0):
         self.name = name
         self.types = types
         self.rarity = rarity
         self.mana_cost = mana_cost
         self.converted_mana_cost = len(mana_cost)
         self.colors = list(get_colors_from_mana_cost(mana_cost))
+        self.base_attack = base_attack
+        self.base_defence = base_defence
 
     def __str__(self):
         return f"""{self.name} / {self.types}
@@ -51,16 +53,21 @@ Colors: {self.colors}
 
 
 class Ability:
-    def __init__(self, name: str, mana_cost: List[ManaPoint], tapping: bool):
+    """A class abstracting a non-static ability a card has."""
+    def __init__(self, name: str, description: str, mana_cost: List[ManaPoint], tapping: bool):
         self.name = name
+        self.description = description
         self.mana_cost = mana_cost
         self.tapping = tapping
 
 
 def get_colors_from_mana_cost(mana_cost: Iterable[ManaPoint]) -> Set[Color]:
-    unique_colors = set()
+    """A function which converts an iterable collection of ManaPoint objects into a set of unique colors an enitity
+    has. If the passed collection is empty, the returned value is a set with a single Color.ANY value."""
+    unique_colors = {Color.ANY}
     for point in mana_cost:
         if point.colors and len(point.colors) == 1:
             unique_colors.add(point.colors[0])
-    unique_colors.discard(Color.ANY)
+    if len(unique_colors) > 1:
+        unique_colors.discard(Color.ANY)
     return unique_colors
--- main.py
+++ main.py
@@ -14,9 +14,34 @@ def create_hydras_growth():
     )
 
 
+def create_broodmate_dragon():
+    return Card(
+        "Broodmate Dragon",
+        [ManaPoint([Color.GREEN]), ManaPoint([Color.RED]), ManaPoint([Color.BLACK]),
+         ManaPoint([Color.ANY]), ManaPoint([Color.ANY]), ManaPoint([Color.ANY])],
+        [CardType.PERMANENT_CREATURE],
+        CardRarity.RARE
+    )
+
+
+def create_black_lotus():
+    return Card(
+        "Black Lotus",
+        [],
+        [CardType.PERMANENT_ENCHANTMENT],
+        CardRarity.BONUS
+    )
+
+
 if __name__ == '__main__':
     card1 = create_jungle_delver()
     card2 = create_hydras_growth()
+    card3 = create_broodmate_dragon()
+    card4 = create_black_lotus()
     print(card1)
     print()
     print(card2)
+    print()
+    print(card3)
+    print()
+    print(card4)
